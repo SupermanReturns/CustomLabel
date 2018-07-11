54188a4fb9648757727d28ac565962320b36517a
Superman <703353105@qq.com>
2018-07-11 17:06:45 +0800

    Initial Commit

---
+++ CustomLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomLabel
+//
+//  Created by Superman on 2018/7/11.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
